3601b57a6c7a8f0854c4f941eeb88920005de94b
NamsooCho <nscho66@gmail.com>
2019-04-12 16:52:21 +0900

    feat(stats): replace Mutex<u64> with AtomicU64

--- src/toxcore/stats.rs
+++ src/toxcore/stats.rs
@@ -4,10 +4,7 @@ This is used by both Udp codec and Tcp codec.
 */
 
 use std::sync::Arc;
-#[cfg(target_pointer_width = "64")]
 use std::sync::atomic::*;
-#[cfg(not(target_pointer_width = "64"))]
-use std::sync::Mutex;
 
 /// Struct for various counters
 #[derive(Clone, Default)]
@@ -38,58 +35,30 @@ pub struct Counters {
 /// Struct for counting packets on non-x64 CPU.
 pub struct Counters {
     /// Incoming packets count for Udp/Tcp
-    incoming: Mutex<u64>,
+    incoming: AtomicU64,
     /// Outgoing packets count for Udp/Tcp
-    outgoing: Mutex<u64>,
+    outgoing: AtomicU64,
 }
 
 impl Counters {
     /// Add 1 to incoming counter
-    #[cfg(target_pointer_width = "64")]
     pub fn increase_incoming(&self) {
         self.incoming.fetch_add(1, Ordering::Relaxed);
     }
 
-    /// Add 1 to incoming counter
-    #[cfg(not(target_pointer_width = "64"))]
-    pub fn increase_incoming(&self) {
-        *self.incoming.lock().expect("Can't lock mutex") += 1;
-    }
-
     /// Add 1 to outgoing counter
-    #[cfg(target_pointer_width = "64")]
     pub fn increase_outgoing(&self) {
         self.outgoing.fetch_add(1, Ordering::Relaxed);
     }
 
-    /// Add 1 to outgoing counter
-    #[cfg(not(target_pointer_width = "64"))]
-    pub fn increase_outgoing(&self) {
-        *self.outgoing.lock().expect("Can't lock mutex") += 1;
-    }
-
     /// Get incoming counter
-    #[cfg(target_pointer_width = "64")]
     pub fn incoming(&self) -> u64 {
         self.incoming.load(Ordering::Relaxed) as u64
     }
 
-    /// Get incoming counter
-    #[cfg(not(target_pointer_width = "64"))]
-    pub fn incoming(&self) -> u64 {
-        *self.incoming.lock().expect("Can't lock mutex")
-    }
-
     /// Get outgoing counter
-    #[cfg(target_pointer_width = "64")]
     pub fn outgoing(&self) -> u64 {
         self.outgoing.load(Ordering::Relaxed) as u64
-    }
-
-    /// Get outgoing counter
-    #[cfg(not(target_pointer_width = "64"))]
-    pub fn outgoing(&self) -> u64 {
-        *self.outgoing.lock().expect("Can't lock mutex")
     }
 }
 
